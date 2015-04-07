a735c0b95e5439b11c5ab4a06519387fc2903b2a
Max Mansfield <max.m.mansfield@gmail.com>
2015-04-07 14:43:05 -0600

    This commit separates the source and the built site into different branches so that github will not rebuild the optimized pages.

--- Gruntfile.js
+++ Gruntfile.js
@@ -8,15 +8,27 @@ module.exports = function( grunt ) {
                 options : {
                     import : 2
                 },
-                src : [ 'assets/themes/bootstrap/css/style.css',
-                'assets/themes/bootstrap/css/snippets.css' ]
+                src : [ 'assets/themes/bootstrap/css/style.css' ]
             }
         },
 
+            
         concat : {
-            dist : {
-                src : [ 'assets/themes/bootstrap/css/style.css', 
-                'assets/themes/bootstrap/css/snippets.css'],
+            js : {
+                src : [ 'assets/themes/bootstrap/resources/jquery/jquery-2.1.1.min.js',
+                        'assets/themes/bootstrap/resources/bootstrap/js/bootstrap.min.js',
+                        'assets/themes/bootstrap/resources/material/js/ripples.min.js',
+                        'assets/themes/bootstrap/resources/material/js/material.min.js'  
+                      ],
+                dest : 'assets/themes/bootstrap/main.js'
+            },
+            css : {
+                src : [ 'assets/themes/bootstrap/resources/material/css/material.min.css',
+                        'assets/themes/bootstrap/resources/material/css/material-wfont.min.css',
+                        'assets/themes/bootstrap/resources/material/css/ripples.min.css',
+                        'assets/themes/bootstrap/css/style.css',
+                        'assets/themes/bootstrap/resources/bootstrap/css/bootstrap-paper.min.css'  
+                      ],
                 dest : 'assets/themes/bootstrap/css/main.css'
             }
         },
@@ -28,6 +40,9 @@ module.exports = function( grunt ) {
             },
             jekyllServe : {
                 command : 'jekyll serve'
+            },
+             clean : {
+                command : 'rm -r _site fi'
             }
         },
 
@@ -36,12 +51,97 @@ module.exports = function( grunt ) {
             files: [{
               expand: true,
               cwd: 'assets/themes/bootstrap/css/',
-              src: ['*.css', '!*.min.css'],
+              src:  'main.css',
               dest: 'assets/themes/bootstrap/css/',
               ext: '.min.css'
           }]
       }
   },
+   uglify: {
+    options: {
+      mangle: false
+    },
+    my_target: {
+      files: {
+        'assets/themes/bootstrap/main.min.js': ['assets/themes/bootstrap/main.js']
+      }
+    }
+  },
+   svgmin: {
+      dist: {
+        files: [{
+          expand: true,
+          cwd: 'assets/images/svg',
+          src: ['**/*.svg', '!*binary_overlay.svg'],
+          dest: 'assets/images/svg'
+        }]
+      }
+    },
+
+
+    imageoptim: {
+      options: {
+        quitAfter: true
+      },
+      distLossless: {
+        options: {
+          jpegMini: false,
+          imageAlpha: true
+        },
+        files: [{
+          expand: true,
+          cwd: 'assets/images/',
+          src: '**/*.{gif,png}',
+          dest: 'assets/images/'
+        }]
+      },
+      distLossy: {
+        options: {
+          jpegMini: false,
+          imageAlpha: true
+        },
+        files: [{
+          expand: true,
+          cwd: 'assets/images/jpg',
+          src: '**/*.{jpg,jpeg}',
+          dest: 'assets/images/jpg'
+        }]
+      }
+    },
+
+pagespeed: {
+      options: {
+        locale: 'en_US',
+        nokey: true,
+        url: 'http://bytewise.io'
+      },
+      desktop: {
+        options: {
+          strategy: 'desktop'
+        }
+      },
+      mobile: {
+        options: {
+          strategy: 'mobile'
+        }
+      }
+    },
+
+   phantomas: {
+      prod: {
+        options: {
+          indexPath: 'about/perf/phantomas/',
+          options   : {
+            'film-strip': false,
+            'no-externals': true,
+            'timeout': 60
+          },
+          url: 'http://bytewise.io/',
+          buildUi: true
+        }
+      }
+
+    },
 
   htmlmin: {
       dist: {
@@ -55,34 +155,21 @@ module.exports = function( grunt ) {
       },
       files: [{
           expand: true,
-          cwd: ['_site/' ],
-          src: ['*.html'],
-          dest: '_site/'
+          cwd: '_site' ,
+          src: ['**/*.html', '!about/perf/phantomas/**/*.html' ],
+          dest: '_site'
       }]
   }
 },
-csscomb: {
-    dist: {
-        options: {
-            config: '.csscomb.json'
-        },
-        files: {
-            'assets/themes/bootstrap/css/main-sorted.css': ['assets/themes/bootstrap/css/main.css']
-        }
-    }
-},
-
-prettify: {
-
-  // Prettify a directory of files 
-  all: {
-    expand: true,
-    cwd: './',
-    ext: '.html',
-    src: ['**/*.html'],
-    dest: './'
-  }
-},
+    penthouse: {
+      server : {
+        outfile : '_include/css/critical.css',
+        css : 'assets/themes/bootstrap/css/main.css',
+        url : 'http://localhost:4000',
+        width : 1280,
+        height : 800
+      }
+    },
 
 watch : {
     files : [ '_layouts/*.html',
@@ -106,11 +193,21 @@ watch : {
 
     // register custom grunt tasks
 
-    grunt.registerTask( 'clean', [ 'prettify'] );
-    grunt.registerTask( 'lint', [ 'csslint'] );
-    grunt.registerTask( 'test', [ 'lint' ] );
-    grunt.registerTask( 'optimize', [ 'concat', 'csscomb', 'cssmin', 'htmlmin'] );
-    //grunt.registerTask( 'deploy', [ ] )
+
+
+
+
+
+
+    grunt.registerTask( 'prebuild', [ 'concat','penthouse', 'svgmin', 'imageoptim' ]);
+    grunt.registerTask( 'build', ['prebuild', 'shell:jekyllBuild'] );
+
+    grunt.registerTask( 'minimize', ['uglify', 'cssmin', 'htmlmin'] );
+
+    grunt.registerTask( 'csslint', [ 'csslint'] );
+    grunt.registerTask( 'test', ['phantomas', 'pagespeed'] );
+
+    grunt.registerTask( 'deploy', [ 'build', 'minimize', 'test']);
 };
 
 
--- _site/README.md
+++
@@ -1,5 +0,0 @@
-#Bytewise.io
-## Home of Denvers most elite freelance group and developer blog.
-
-![Travis-CI](https://travis-ci.org/MaxMansfield/maxmansfield.github.io.svg?branch=master)
-
