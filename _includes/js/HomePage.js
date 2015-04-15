f7e8d55a2fab7b00de07a498ada87c4b3a7033f2
Max Mansfield <max.m.mansfield@gmail.com>
2015-04-15 15:43:43 -0600

    Added to the index page and completed parallax of panels 0,1 and 2

--- _includes/js/HomePage.js
+++ _includes/js/HomePage.js
@@ -1,4 +1,4 @@
- divisor = 5;
+ divisor = 1;
 
  var controller = new ScrollMagic.Controller({
       globalSceneOptions: {
@@ -7,10 +7,16 @@
     });
 
 
-  new ScrollMagic.Scene({triggerElement: "#logo_trigger", duration: "35%"})
+  new ScrollMagic.Scene({triggerElement: "#logo_trigger", duration: "35%", offset: "-60%"})
                 .setTween(logoSlideIn)
                 .addTo(controller);
 
-  new ScrollMagic.Scene({triggerElement: "#header_pin_trigger", duration: "45%", offset: "-40%"})
+  new ScrollMagic.Scene({triggerElement: "#header_pin_trigger", duration: "32%", offset: "-40%"})
                 .setPin("#header_pin", {pushFollowers: false})
                 .addTo(controller);
+
+   new ScrollMagic.Scene({triggerElement: "#indimg-2-trigger", duration: "500%"})
+                .setPin("#indimg-1", {pushFollowers: false})
+                .addTo(controller);
+
+              
